90b42e592ad692f1644f1de0e8653c53acd2cafe
mokhammad-kodehode <125983627+mokhammad-kodehode@users.noreply.github.com>
2023-08-09 09:44:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
+// Define the character sets for generating passwords
+
 let letters = "abcdefghijklmnopqrstuvwxyz";
 let symbols = "~!@#$%^&*_-+={[}]|:;<>.?/";
 let numbers = "123456789";
 let chars = letters;
 
+// Function to generate a password based on selected options
+
 function generatorPassword(size) {
+  // Include symbols if the corresponding checkbox is checked
   if (document.querySelector("#checksym").checked) chars += symbols;
+
+  // Include numbers if the corresponding checkbox is checked
   if (document.querySelector("#checknum").checked) chars += numbers;
 
   let password = "";
+  // Generate password of the given size using the selected character set
   for (let i = 0; i < size; i++) {
     const randomIdx = Math.floor(Math.random() * chars.length);
     password += chars[randomIdx];
   }
 
+  // Capitalize the first letter if the corresponding checkbox is checked
   if (document.querySelector("#checkcap").checked) {
     password = password[0].toUpperCase() + password.slice(1);
   }
 
+  // Replace the last character with an exclamation mark if checkbox is checked
   if (document.querySelector("#checkonesym").checked) {
     password = password.slice(0, -1) + "!";
   }
@@ -33,26 +43,19 @@ genPassword.addEventListener("click", function () {
   for (const passout of passouts) passout.textContent = generatorPassword(size);
 });
 
+// Function to copy text to clipboard
 function copy(text) {
   navigator.clipboard.writeText(text).then(() => {
     let copyDone = document.querySelector("#copySuccess");
     copyDone.style.display = "block";
-    setTimeout(() => (copyDone.style.display = "none"), 1000);
+    copyDone.style.setTimeout(() => (copyDone.style.display = "none"), 1000);
   });
 }
+
+// Add click event listener to each password output element for copying
 
 for (const passout of passouts) {
   passout.addEventListener("click", function () {
     copy(this.textContent);
   });
 }
-
-// Generate two random passwords when the user clicks the button
-
-// Each password should be 15 characters long
-
-// Ability to set password lenght
-
-// Add copy on click
-
-// Toggle "symbols" and " numbers" on /off
